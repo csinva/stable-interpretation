2aba7a222e8bed805ed0689a5884d0997d23f235
Chandan Singh <chandan_singh@berkeley.edu>
2020-09-24 16:05:34 -0700

    put gradient into attr function

--- src/attributions.py
+++ src/attributions.py
@@ -6,23 +6,25 @@ import sys
 from numpy.fft import *
 from torch import nn
 from captum.attr import *
-
+from . import viz
 
 def get_attributions(x_t: torch.Tensor, 
                      mt, 
                      class_num=1,
-                     attr_methods = ['IG', 'DeepLift', 'SHAP', 'CD', 'InputXGradient'],
+                     attr_methods = ['Gradient', 'IG', 'DeepLift', 'SHAP', 'CD', 'InputXGradient'],
                      device='cuda'):
-    '''Returns all scores in a dict assuming mt works with both grads + CD
+    '''Returns attribution maps in a dictionary for different attribution methods
 
     Params
     ------
-    mt: model
-    class_num: target class
+    mt: pytorch model
+    class_num: int
+        target class
     
-    Return
+    Returns
+    -------
     results: dict
-        Dictionary of attributions
+        Dictionary of attribution maps
     '''
     x_t = x_t.to(device)
     x_t.requires_grad = True
@@ -30,12 +32,17 @@ def get_attributions(x_t: torch.Tensor,
     mt.eval()
 
     results = {}
-    if 'CD' in attr_methods:
-        attr_funcs = [IntegratedGradients, DeepLift, GradientShap, None, InputXGradient]
-    else:
-        attr_funcs = [IntegratedGradients, DeepLift, GradientShap, InputXGradient]
+    attr_funcs_dict = {
+        'Gradient': None,
+        'IG': IntegratedGradients,
+        'DeepLift': DeepLift,
+        'SHAP': GradientShap,
+        'CD': None,
+        'InputXGradient': InputXGradient
+    }
         
-    for name, func in zip(attr_methods, attr_funcs):
+    for name in attr_methods:
+        func = attr_funcs_dict[name]
         if name == 'CD':
             with torch.no_grad():
                 sweep_dim = 1
@@ -43,8 +50,11 @@ def get_attributions(x_t: torch.Tensor,
                 if x_t.shape[-1] == 2: # check for imaginary representations
                     tiles = np.repeat(np.expand_dims(tiles, axis=-1), repeats=2, axis=3).squeeze()
                 tiles = torch.Tensor(tiles).unsqueeze(1)
-                attributions = acd.get_scores_2d(mt, method='cd', ims=tiles, im_torch=x_t)[..., class_num].T.reshape(-1,28,28).squeeze()
+                attributions = acd.get_scores_2d(mt, method='cd', ims=tiles, im_torch=x_t)[..., class_num].T.reshape(-1, 28, 28).squeeze()
                 # attributions = score_funcs.get_scores_2d(mt, method='cd', ims=tiles, im_torch=x_t)[..., class_num].T.reshape(-1,28,28)
+        elif name == 'Gradient':
+            mt(x_t)[0, class_num].backward() # calculate gradients
+            attributions = viz.detach(x_t.grad)
         else:
             baseline = torch.zeros(x_t.shape).to(device)
             attributer = func(mt)
